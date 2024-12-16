d478eef14c95367686c0e1ed3af6af9158cec669
Sieep-Coding <nstamb6@gmail.com>
2024-12-16 16:02:18 -0500

    Book constructor

---
+++ Classes/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace TCSA.OOP.LibraryManagementSystem.Classes
+{
+    internal class Book
+    {
+        string Name;
+        int Pages;
+        internal Book(string name, int pages)
+        {
+            Name = name;
+            Pages = pages;
+            Console.WriteLine($"Name: {Name}, Pages: {Pages}");
+        }
+    }
+}
