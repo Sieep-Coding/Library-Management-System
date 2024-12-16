98e7fd913f1369af27f19f6907a182bd69c37b5c
Sieep-Coding <nstamb6@gmail.com>
2024-12-16 12:10:44 -0500

    refactor file structure

--- Models/MenuOption.cs
+++ Models/MenuOption.cs
@@ -1,11 +1,4 @@
 ﻿// Ignore Spelling: TCSA OOP
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TCSA.OOP.LibraryManagementSystem.Models
 {
     internal static class MenuOption
