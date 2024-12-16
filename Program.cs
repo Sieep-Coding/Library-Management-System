dee1e716c4bb391812fb126fe20209c09a168d0e
Sieep-Coding <nstamb6@gmail.com>
2024-12-15 23:14:06 -0500

    adding logic for console app

--- Program.cs
+++ Program.cs
@@ -38,13 +38,37 @@ while (true)
     {
         case "View Book":
             AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
+
             foreach (var book in books)
             {
                 AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
             }
+
+            AnsiConsole.MarkupLine("Press any key to continue.");
+
+            Console.ReadKey();
+
             break;
+
         case "Add Book":
+            var title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the book to add:");
+
+            if (books.Contains(title))
+            {
+                AnsiConsole.MarkupLine("[red]This book already exists.[/]");
+            }
+
+            else
+            {
+                books.Add(title);
+                AnsiConsole.MarkupLine("[green]Book added successfully![/]");
+            }
+
+            AnsiConsole.MarkupLine("Press any key to continue.");
+
+            Console.ReadKey();
             break;
+
         case "Delete Book":
             break;
     }
